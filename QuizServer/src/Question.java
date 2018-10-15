65d3f6048cefa0741b62bbb3d287d0cd1f586b58
NickBoe <NickBoe@users.noreply.github.com>
2018-10-15 14:07:37 +0200

    Questions

---
+++ QuizServer/src/Question.java
@@ -0,0 +1,65 @@
+
+public class Question {
+	int a = 1;
+	
+	/*Question 1: What is the capital of Denmark
+		A: Oslo
+		B: Reykjavik
+		C: Copenhagen - correct
+		D: London	
+	
+	Question 2: How old is the Earth? 
+		A: 4.5 billion years old - correct
+		B: 3.5 billion years old
+		C: 4 billion years old
+		D: 5 billion years old
+	
+	Question 3: 5 multiplied by 4 is what?
+		A: 10
+		B: 20 - correct
+		C: 25
+		D: 15
+	
+	Question 4: What is the name of the four nucleobases in DNA
+		A: bromine, guanine, cytosine and thymine
+		B: chlorine, thymine, kerosine and adenine
+		C: deoxine, cytosine, guanine and thymine
+		D: adenine, guanine, cytosine and thymine - correct
+	
+	Question 5: What is Supermans alter ego?
+		A: John Smith
+		B: Mike Jackson
+		C: Clark Kent - correct
+		D: Bruce Wayne
+	
+	Question 6: Who is the current president of the United States of America?
+		A: Donald Trump - correct 
+		B: Barack Obama
+		C: George W. Bush
+		D: Hillary Clinton
+	
+	Question 7: How many strings does a guitar have?
+		A: 2
+		B: 4
+		C: 6 - correct
+		D: 8
+	
+	Question 8: How fast are you usually allowed to drive within the city limits?
+		A: 50 km/t - correct
+		B: 60 km/t
+		C: 30 km/t
+		D: 40 km/t
+	
+	Question 9: Which of these sports do NOT use a raquet
+		A: Polo - correct
+		B: Badminton
+		C: Squash
+		D: Tennis
+	
+	Question 10: Does the moon rotate?
+		A: No - correct
+		B: Yes, but only during the day
+		C: Yes
+		D: Yes, but only during the summer months
+*/
+}
---
+++ QuizServer/src/QuizServer.java
@@ -0,0 +1,5 @@
+
+public class QuizServer {
+
+	int a = 1;
+}
