1b956bef33a577df27e15654c27e963d93cc10a4
NickBoe <NickBoe@users.noreply.github.com>
2018-10-16 11:22:15 +0200

    QuestionDB class + Question class constructor

--- QuizServer/src/Question.java
+++ QuizServer/src/Question.java
@@ -8,78 +8,12 @@ public class Question {
 	public String D;
 	public String Ans;
 	
-	public Question() {
+	public Question(String QuizText, String A, String B, String C, String D, String Ans) {
+		this.QuizText = QuizText;
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+		this.Ans = Ans;
 	}
-	
-	/*public String[] QuestionText = {"What is the capital of Denmark?", "How old is the Earth?", "5 multiplied by 4 is what?", "What is the name of the four nucleobases in DNA?",
-			"What is Supermans alter ego?", "Who is the current president of the United States of America?", "How many strings does a guitar have?",
-			"How fast are you usually allowed to drive within the city limits?", "Which of these sports do NOT use a racket", "Does the moon rotate?"};
-	
-	public String[] PosAnswerQ1 = {"Olso", "Rekjavik", "Copenhagen", "London"};
-	public String[] PosAnswerQ2 = {"4.5 billion years old", "3.5 billion years old","4 billion years old","5 billion years old"};
-	
-	public String[]
-	
-	//Make question array with answer options and correct answer
-	
-	/*Question 1: What is the capital of Denmark
-		A: Oslo
-		B: Reykjavik
-		C: Copenhagen - correct
-		D: London	
-	
-	Question 2: How old is the Earth? 
-		A: 4.5 billion years old - correct
-		B: 3.5 billion years old
-		C: 4 billion years old
-		D: 5 billion years old
-	
-	Question 3: 5 multiplied by 4 is what?
-		A: 10
-		B: 20 - correct
-		C: 25
-		D: 15
-	
-	Question 4: What is the name of the four nucleobases in DNA
-		A: bromine, guanine, cytosine and thymine
-		B: chlorine, thymine, kerosine and adenine
-		C: deoxine, cytosine, guanine and thymine
-		D: adenine, guanine, cytosine and thymine - correct
-	
-	Question 5: What is Supermans alter ego?
-		A: John Smith
-		B: Mike Jackson
-		C: Clark Kent - correct
-		D: Bruce Wayne
-	
-	Question 6: Who is the current president of the United States of America?
-		A: Donald Trump - correct 
-		B: Barack Obama
-		C: George W. Bush
-		D: Hillary Clinton
-	
-	Question 7: How many strings does a guitar have?
-		A: 2
-		B: 4
-		C: 6 - correct
-		D: 8
-	
-	Question 8: How fast are you usually allowed to drive within the city limits?
-		A: 50 km/t - correct
-		B: 60 km/t
-		C: 30 km/t
-		D: 40 km/t
-	
-	Question 9: Which of these sports do NOT use a racket
-		A: Polo - correct
-		B: Badminton
-		C: Squash
-		D: Tennis
-	
-	Question 10: Does the moon rotate?
-		A: No - correct
-		B: Yes, but only during the day
-		C: Yes
-		D: Yes, but only during the summer months
-*/
 }
